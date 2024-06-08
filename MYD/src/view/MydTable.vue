56bb67fbbd5dcdf28630b5ffa4f260e8ecfa75f1
Neflibata-z <fairy@myfairytale.cn>
2024-06-08 22:13:01 +0800

    main

--- MYD/src/view/MydTable.vue
+++ MYD/src/view/MydTable.vue
@@ -1,21 +1,22 @@
 <template>
     <div class="common-layout">
-<!--         <el-container>
+        <el-container>
             <el-header>
-                MYD
-                <el-form>
+                <el-form ref="formRef">
                     <el-form-item label="Activity zone">
-                        <el-select v-model="sizeForm.region" placeholder="please select your zone">
-                            <el-option label="Zone one" value="shanghai" />
-                            <el-option label="Zone two" value="beijing" />
-                        </el-select>
-                    </el-form-item>
-                    <el-form-item>
-                        <el-button type="primary" @click="submitForm(formRef)">Submit</el-button>
+                        <el-cascader v-model="value" :options="options" :props="props"
+                            @change="handleChange" /></el-form-item><el-form-item>
+                        <el-button type="primary" @click="submitForm()">Submit</el-button>
                         <el-button @click="resetForm(formRef)">Reset</el-button>
                     </el-form-item></el-form>
             </el-header>
-            <el-main><el-table :data="tableData" style="width: 100%">
+            <el-main>
+                <div>统计表</div>
+                <el-table :data="table.tableData" style="width: 100%" v-if="table.tableShow">
+                    <el-table-column v-for="item in table.tableHeader" :prop="item.date" :label="item.title"
+                        width="150" />
+                </el-table>
+                <!--                 <el-table :data="tableData" style="width: 100%">
                     <el-table-column prop="date" label="Date" width="150" />
                     <el-table-column label="Delivery Info">
                         <el-table-column prop="name" label="Name" width="120" />
@@ -26,10 +27,52 @@
                             <el-table-column prop="zip" label="Zip" width="120" />
                         </el-table-column>
                     </el-table-column>
-                </el-table></el-main>
-        </el-container> -->
+                </el-table> -->
+            </el-main>
+        </el-container>
     </div>
 </template>
 <script setup lang="ts">
-
+import { reactive, ref } from 'vue'
+import type { ComponentSize, FormInstance, FormRules } from 'element-plus'
+const props = {
+    expandTrigger: 'hover' as const,
+}
+const value = ref([])
+const options = [
+    {
+        value: 'guide',
+        label: 'Guide',
+        children: [
+            {
+                value: 'disciplines',
+                label: 'Disciplines',
+                children: [
+                    {
+                        value: 'consistency',
+                        label: 'Consistency',
+                    },
+                ],
+            },]
+    }]
+const formRef = ref<FormInstance>()
+const table: any = reactive({
+    tableShow: false,
+    tableHeader: [{ date: 'name', title: 'Name' }, { date: 'state', title: "State" }], 
+    tableData: [{ name: 'z', state: '1' }]
+})
+//const tableHeader:any=[{date:'name',title:'Name'},{date:'state',title:"State"}];
+//const tableData:any = [{name:'z',date:'1'}];
+//const tableShow:boolean=false;
+const handleChange = (val: any) => {
+    console.log(val);
+}
+const resetForm = (formEl: FormInstance | undefined) => {
+    if (!formEl) return
+    formEl.resetFields();
+    table.tableShow = false;
+}
+const submitForm = () => {
+    table.tableShow = true;
+}
 </script>
